00a19ac0888f2170e47e8127e774c96f63e41a9e
pras75299 <singh.prashantking@gmail.com>
2021-04-18 15:17:15 +0530

    add tab button active logic while value of button match index

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
         {
           jobs.map((item, index) => {
             return <button key={item.id} 
-            onClick={() => setValue(index)}>
+            onClick={() => setValue(index)}  className={`job-btn ${index === value && 'active-btn'}`}>
               {item.company}</button>
           })
         }
